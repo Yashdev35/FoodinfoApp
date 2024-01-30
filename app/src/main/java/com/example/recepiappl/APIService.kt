f88af8866b633ffcc40ac435169e918b892225b1
Yash VIjay Kumbhkarn <cse230001083@iiti.ac.in>
2024-01-30 18:34:10 +0530

    uploading to git hub

--- app/src/main/java/com/example/recepiappl/APIService.kt
+++ app/src/main/java/com/example/recepiappl/APIService.kt
@@ -3,6 +3,7 @@ package com.example.recepiappl
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+
 private val retrofit = Retrofit.Builder()
     .baseUrl("https://www.themealdb.com/api/json/v1/1/")
     .addConverterFactory(GsonConverterFactory.create()).build()
